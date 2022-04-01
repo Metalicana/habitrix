09bae63383328abfa5825de260ba63ae829e577a
turno99 <turnoshovon1999@gmail.com>
2022-04-01 13:28:15 +0600

    Added task data class, and made minor refactoring.

---
+++ lib/models/task.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TaskAdapter extends TypeAdapter<Task> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Task read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Task(
+      taskId: fields[0] as String,
+      taskName: fields[1] as String,
+      deadline: fields[2] as DateTime,
+      difficulty: fields[3] as int,
+      importance: fields[4] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Task obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.taskId)
+      ..writeByte(1)
+      ..write(obj.taskName)
+      ..writeByte(2)
+      ..write(obj.deadline)
+      ..writeByte(3)
+      ..write(obj.difficulty)
+      ..writeByte(4)
+      ..write(obj.importance);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
