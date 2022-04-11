8d0370d7c6adaffeb1a0632c52aa6bbf334d9695
Metalicana <alradi9923@gmail.com>
2022-04-11 22:06:08 +0600

    visualization screen template added

--- lib/screens/habit/habitList/habitListScreen.dart
+++ lib/screens/habit/habitList/habitListScreen.dart
@@ -17,45 +17,38 @@ import 'habitList.dart';
 class HabitListScreen extends StatelessWidget {
   final AuthService _auth = AuthService();
 
-
   @override
   Widget build(BuildContext context) {
-
     return Container(
       child: Scaffold(
         backgroundColor: Color(0xFFf4f7f2),
         appBar: AppBar(
-          shape: RoundedRectangleBorder(
+          shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.vertical(
               bottom: Radius.circular(50.0),
             ),
           ),
-          title: const Text(
-              'Habits',
+          title: const Text('Habits',
               style: TextStyle(
                 fontSize: 30.0,
-              )
-          ),
+              )),
           toolbarHeight: 220,
           backgroundColor: kPrimaryColor,
-
         ),
-    body: ValueListenableBuilder(
-          valueListenable: Hive.box<Habit>(HiveBoxes.habit).listenable(),
-          builder: (BuildContext context, Box<Habit> box, _) {
-            return HabitList( box: box);
-          }
-       ),
-           floatingActionButton: FloatingActionButton(
-           backgroundColor: kPrimaryColor,
+        body: ValueListenableBuilder(
+            valueListenable: Hive.box<Habit>(HiveBoxes.habit).listenable(),
+            builder: (BuildContext context, Box<Habit> box, _) {
+              return HabitList(box: box);
+            }),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: kPrimaryColor,
             child: const Icon(Icons.add),
-            onPressed: (){
+            onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const HabitAddForm()),
               );
-            }
-        ),
+            }),
       ),
     );
   }
